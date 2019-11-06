76902a65434c9f53ce36002dea565be79834dc85
Adam Baxter <adam8690@hotmail.co.uk>
2019-11-06 23:01:06 +0000

    add an api to store schedule

---
+++ ScheduleApi/Controllers/ScheduleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ScheduleApi.Storage;
+
+namespace ScheduleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScheduleController : ControllerBase
+    {
+
+        private readonly ILogger<ScheduleController> _logger;
+        private readonly FileWriter _writer;
+
+        public ScheduleController(ILogger<ScheduleController> logger, FileWriter writer)
+        {
+            _logger = logger;
+            _writer = writer;
+        }
+
+        [HttpGet]
+        public Schedule Get()
+        {
+            return _writer.ReadFromJsonFile();
+        }
+
+        [HttpPost]
+        public StatusCodeResult Post([FromBody] Schedule schedule)
+        {
+            _writer.WriteToJsonFile(schedule);
+            return new StatusCodeResult(200);
+        }
+    }
+}
---
+++ ScheduleApi/Schedule.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace ScheduleApi
+{
+    public class Schedule
+    {
+        public Dictionary<int, int> MondayTimes { get; set; }
+        //public Dictionary<int, int> TuesdayTimes { get; set; }
+        //public Dictionary<int, int> WednesdayTimes { get; set; }
+        //public Dictionary<int, int> ThursdayTimes { get; set; }
+        //public Dictionary<int, int> FridayTimes { get; set; }
+        //public Dictionary<int, int> SaturdayTimes { get; set; }
+        //public Dictionary<int, int> SundayTimes { get; set; }
+    }
+}
