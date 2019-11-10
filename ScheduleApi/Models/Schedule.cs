03d8a80d477a9a851dd9375d2e3aa4a8ff71fbdc
Adam Baxter <adam8690@hotmail.co.uk>
2019-11-10 22:41:22 +0000

    add boost duration to schedule

--- ScheduleApi/Models/Schedule.cs
+++ ScheduleApi/Models/Schedule.cs
@@ -7,18 +7,20 @@ namespace ScheduleApi
     public class Schedule : ISchedule
     {
         [Required]
-        public Dictionary<int, int> MondayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> MondayTimes { get; set; }
         [Required]
-        public Dictionary<int, int> TuesdayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> TuesdayTimes { get; set; }
         [Required]
-        public Dictionary<int, int> WednesdayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> WednesdayTimes { get; set; }
         [Required]
-        public Dictionary<int, int> ThursdayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> ThursdayTimes { get; set; }
         [Required]
-        public Dictionary<int, int> FridayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> FridayTimes { get; set; }
         [Required]
-        public Dictionary<int, int> SaturdayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> SaturdayTimes { get; set; }
         [Required]
-        public Dictionary<int, int> SundayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> SundayTimes { get; set; }
+        [Range(10, int.MaxValue)]
+        public int BoostDurationMinutes { get; set; } = 15;
     }
 }
--- TimeClock/Models/Schedule.cs
+++ TimeClock/Models/Schedule.cs
@@ -5,12 +5,13 @@ namespace TimeClock
 {
     public class Schedule : ISchedule
     {
-        public Dictionary<int, int> MondayTimes { get; set; }
-        public Dictionary<int, int> TuesdayTimes { get; set; }
-        public Dictionary<int, int> WednesdayTimes { get; set; }
-        public Dictionary<int, int> ThursdayTimes { get; set; }
-        public Dictionary<int, int> FridayTimes { get; set; }
-        public Dictionary<int, int> SaturdayTimes { get; set; }
-        public Dictionary<int, int> SundayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> MondayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> TuesdayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> WednesdayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> ThursdayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> FridayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> SaturdayTimes { get; set; }
+        public Dictionary<int, Dictionary<int, bool>> SundayTimes { get; set; }
+        public int BoostDurationMinutes { get; set; }
     }
 }
